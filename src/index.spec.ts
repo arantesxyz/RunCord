a45966750369f7935bdef0e54067b118640604d8
Gustavo Arantes <me@arantes.dev>
2020-09-30 01:13:40 -0300

    Update package

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,8 +3,3 @@ test("Checks if tests are working", () => {
 
   expect(str).toBeDefined();
 });
-
-import { sum } from "./module";
-test("checks if module import is working", () => {
-  expect(sum(1, 5)).toEqual(6);
-});
