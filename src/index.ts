50b75998af72e2e598e0cc5bc67b7bcd6d5bc942
Gustavo Arantes <me@arantes.dev>
2020-10-11 20:42:17 -0300

    Export data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
 import { Client } from "./Client";
-import { Command } from "./commands/Command";
 
-export { Client, Command };
+import { Command } from "./commands/Command";
+import { CommandManager } from "./commands/CommandHandler";
+import { CommandOptions } from "./commands/structures";
+
+import { InMemoryPromptRepository } from "./prompts/InMemoryPromptRepository";
+import { PromptManager } from "./prompts/PromptManager";
+import {
+  MessagePromptExecutor,
+  Prompt,
+  PromptData,
+  PromptExecutor,
+  PromptQuery,
+  PromptRepository,
+  ReactionPromptExecutor
+} from "./prompts/structures";
+
+export {
+  Client,
+  Command,
+  CommandManager,
+  CommandOptions,
+  PromptManager,
+  InMemoryPromptRepository,
+  MessagePromptExecutor,
+  Prompt,
+  PromptData,
+  PromptExecutor,
+  PromptQuery,
+  PromptRepository,
+  ReactionPromptExecutor
+};
